6a59c69576e6817d50d956bc8993682e4ee83e09
Lucas Fernandes <luks.faf@gmail.com>
2020-06-26 15:00:08 -0300

    Modificando estilização

--- scripts/scriptCountdown.js
+++ scripts/scriptCountdown.js
@@ -4,6 +4,8 @@ const minuts = document.getElementById('minuts');
 const seconds = document.getElementById('seconds');
 
 const inputs = document.getElementById('inputs');
+const back = document.getElementById('back');
+const theme = document.body;
 
 // exit for users
 const hoursTime = document.getElementById('hoursTime');
@@ -16,6 +18,7 @@ var min = 0;
 var sec = 0;
 
 // aux's
+var change = 0;
 var cont = 0;
 var h = 0;
 var m = 0;
@@ -95,6 +98,7 @@ function start(){// start ...
     if(hrs > 0 || min > 0 || sec > 0){// start
       // remove inputs
       inputs.remove();
+      back.remove();
 
       return count();
     }else // 0 -> 0 -> 0
@@ -118,4 +122,27 @@ function restart(){// 00:04:35 -> start -> 00:04:35
   secondsTime.innerHTML = (s < 10 ? '0' + s : s);
 
   return stop();
+}
+
+function mudaCor(){
+
+  if(change === 0){
+    theme.style.backgroundColor = '#fff';
+    hoursTime.style.color = '#000';
+    minutsTime.style.color = '#000';
+    secondsTime.style.color = '#000';
+  }else if(change === 1){
+    theme.style.backgroundColor = '#000'
+    hoursTime.style.color = '#fff';
+    minutsTime.style.color = '#fff';
+    secondsTime.style.color = '#fff';
+  }else{
+    theme.style.backgroundColor = '#f8f8ff';
+    hoursTime.style.color = '#483d8b';
+    minutsTime.style.color = '#483d8b';
+    secondsTime.style.color = '#483d8b';
+    change -= 3;
+  } 
+  change++;
+
 }
